3d164b8f6a7bf1979f04f234604ab44afcc987de
marisayou <marisayou2017@u.northwestern.edu>
2020-10-08 20:36:17 -0700

    Done.

---
+++ lib/project_backer.rb
@@ -0,0 +1,15 @@
+class ProjectBacker
+    attr_reader :project, :backer
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(project, backer)
+        @project = project
+        @backer = backer
+        @@all << self
+    end
+
+end
